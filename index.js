c380bde12d308ba4782171bc6c858e15298f4b24
Michael Albertz <michael.albertz@web.de>
2016-07-08 16:30:22 +0200

    changed formatting: '"' -> '\'' tab -> 2 spaces added space infront of '{' added space around arithmetic operators

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
-var through = require("through"),
-	gutil = require("gulp-util"),
-	request = require("request"),
-	progress = require("request-progress"),
-	col = gutil.colors,
-	ansi = require('ansi'),
-	cursor = ansi(process.stdout),
-	stripAnsi = require('strip-ansi');
+var through = require('through');
+var gutil = require('gulp-util');
+var request = require('request');
+var progress = require('request-progress');
+var ansi = require('ansi');
+var stripAnsi = require('strip-ansi');
+var cursor = ansi(process.stdout);
+var col = gutil.colors;
 
-module.exports = function(urls){
-	var stream = through(function(file,enc,cb){
-		this.push(file);
-		cb();
-	});
+module.exports = function(urls) {
+  var stream = through(function(file,enc,cb) {
+    this.push(file);
+    cb();
+  });
 
+  var files = typeof urls === 'string' ? [urls] : urls;
+  var downloadCount = 0;
 
-	var files = typeof urls === 'string' ? [urls] : urls;
-	var downloadCount = 0;
+  function download(url) {
+    var fileName;
+    var firstLog = true;
+    var msgLen = 0;
 
+    if (typeof url === 'object') {
+      fileName = url.file;
+      url = url.url;
+    } else {
+      fileName = url.split('/').pop();
+    }
 
-	function download(url){
-		var fileName,
-			firstLog = true;
-			msgLen = 0;
+    progress(
+      request({url:url,encoding:null},downloadHandler),
+      {throttle:1000,delay:1000}
+    )
 
-		if (typeof url === "object") {
-			fileName = url.file;
-			url = url.url;
-		} else {
-			fileName = url.split('/').pop();
-		}
-		progress(
-			request({url:url,encoding:null},downloadHandler),
-			{throttle:1000,delay:1000}
-		)
-		.on('progress',function(state){
-			cursor.horizontalAbsolute(msgLen+1).eraseLine().write(Math.ceil(state.percentage * 100)+"%");
-		})
-		.on('data',function(){
-			if(firstLog){
-				var s = '['+col.green('gulp')+']'+' Downloading '+col.cyan(url)+'... ';
-				process.stdout.write(s);
-				firstLog = false;
-				msgLen = stripAnsi(s).length;
-			}
-		});
+    .on('progress', function(state) {
+      cursor.horizontalAbsolute(msgLen + 1).eraseLine().write(Math.ceil(state.percentage * 100) + '%');
+    })
 
-		function downloadHandler(err, res, body){
-			var file = new gutil.File( {path:fileName, contents: new Buffer(body)} );
-			stream.queue(file);
+    .on('data', function() {
+      if(firstLog) {
+        var s = '[' + col.green('gulp')+']' + ' Downloading ' + col.cyan(url) + '... ';
+        process.stdout.write(s);
+        firstLog = false;
+        msgLen = stripAnsi(s).length;
+      }
+    });
 
-			cursor.horizontalAbsolute(msgLen-3).eraseLine().write(col.green(' Complete!\n'));
-			downloadCount++;
-			if(downloadCount != files.length){
-				download(files[downloadCount]);
-			}else{
-				stream.emit('end');
-			}
-		}
-	}
-	download(files[0]);
+    function downloadHandler(err, res, body) {
+      var file = new gutil.File( {path:fileName, contents: new Buffer(body)} );
+      stream.queue(file);
 
-	return stream;
+      cursor.horizontalAbsolute(msgLen-3).eraseLine().write(col.green(' Complete!\n'));
+      downloadCount++;
+      if(downloadCount != files.length) {
+        download(files[downloadCount]);
+      } else {
+        stream.emit('end');
+      }
+    }
+  }
+  download(files[0]);
+
+  return stream;
 };
